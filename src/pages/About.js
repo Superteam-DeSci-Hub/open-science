3e7afe9891998db8c89f9fc60880639a5b8f12b9
miodragstrak <miodrag.strak@gmailcom>
2025-03-17 10:58:23 +0100

    About and Calendar Pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,30 @@ import React from "react";
 import "../styles/home.css";
 
 const About = () => {
-  return <h1 className="home-container">About the DeSci Hub</h1>;
+  return (
+    <div className="about-container">
+      <h1>About the DeSci Hub</h1>
+      <p>
+        The <strong>Superteam DeSci Challenge 2025</strong> is designed to
+        inspire students, faculty members, and researchers to explore and
+        contribute to the scientific discussion on decentralization and
+        blockchain technology, particularly within the <strong>Solana ecosystem</strong>.
+      </p>
+      <p>
+        Participants will have the opportunity to <strong>write and publish research papers</strong> or projects on topics such as
+        decentralization, DeFi, NFTs, MiCA, AML, tokenomics, and other key
+        blockchain subjects.
+      </p>
+      <p>
+        This initiative will establish a <strong>dedicated website</strong> and an
+        <strong> open-access research repository</strong>, fostering a
+        <strong> network of academics and blockchain experts</strong>. The best
+        submissions will receive <strong>awards</strong>, encouraging engagement
+        and innovation.
+      </p>
+
+    </div>
+  );
 };
 
 export default About;
--- src/pages/Calendar.js
+++ src/pages/Calendar.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "../styles/home.css";
 
-const Events = () => {
-  return <h1 className="home-container">Upcoming Events</h1>;
+const Calendar = () => {
+  return(
+    <div className="about-container">
+      <h1>Timeline</h1>
+      <p> 📢 <strong>March:</strong> Official launch and open call for participation.</p>
+      <p> 🔎 <strong>April - September:</strong> Collaboration with students, researchers, and conference organizers.</p>
+      <p> 🏆 <strong>October:</strong> Award ceremony for the best research papers and projects.</p>
+    </div>
+  )
 };
 
-export default Events;
+export default Calendar;
