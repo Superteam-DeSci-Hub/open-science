ea697b31661d6c3fcbee70ac9019ac0e2988f122
miodragstrak <miodrag.strak@gmailcom>
2025-03-27 14:34:37 +0100

    Updates

---
+++ precache-manifest.6cc2d66774f56f209cb74595a96ab58b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "353e471c7880b67a6db049c0dcd76aa0",
+    "url": "/open-science/index.html"
+  },
+  {
+    "revision": "149e30d49f3fd5a9bbec",
+    "url": "/open-science/static/css/2.05cbb0d5.chunk.css"
+  },
+  {
+    "revision": "4b55f4f44a3458848b1d",
+    "url": "/open-science/static/css/main.e382b269.chunk.css"
+  },
+  {
+    "revision": "149e30d49f3fd5a9bbec",
+    "url": "/open-science/static/js/2.06ed0668.chunk.js"
+  },
+  {
+    "revision": "4b55f4f44a3458848b1d",
+    "url": "/open-science/static/js/main.7217f9e7.chunk.js"
+  },
+  {
+    "revision": "c74dd901397d82536153",
+    "url": "/open-science/static/js/runtime~main.145586cb.js"
+  },
+  {
+    "revision": "dcd90f63050d9832586eeb143c6946cf",
+    "url": "/open-science/static/media/logo.dcd90f63.png"
+  },
+  {
+    "revision": "683a619738da515042057ac78901ec02",
+    "url": "/open-science/static/media/news2.683a6197.jpg"
+  },
+  {
+    "revision": "c98ea79bb9c602b55c9912f705b5d76f",
+    "url": "/open-science/static/media/stgrant.c98ea79b.png"
+  }
+]);
